bc37fefb992aae2bd58ffcf8ae3f016b9ed03b99
AnshpreetSingh351 <gujralanshpreetsingh@gmail.com>
2026-03-08 02:29:57 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // 🚀 This creates the 'out' folder Render needs
+  images: {
+    unoptimized: true, // Required for static export images
+  },
 };
 
 export default nextConfig;
